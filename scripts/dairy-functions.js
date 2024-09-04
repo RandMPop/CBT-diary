3a69f71e6ddd0a3c4b01976334470e657e4bf56a
RandMPop <OlgaCherniaeva99@yandex.ru>
2024-09-04 22:35:16 +0500

    Functionality for Emotions table was added. It is now possible to choose emotions and add them to the table.

--- scripts/dairy-functions.js
+++ scripts/dairy-functions.js
@@ -1,16 +1,37 @@
-const infoButtonList = document.querySelectorAll(".user-answer-info");
-const popUpList = document.querySelectorAll(".pop-up-info");
-infoButtonList.forEach((infoButton,i) => {
-    infoButton.addEventListener("click",() =>{
-        popUpList[i].classList.remove("hidden");
+const infoButtonList = document.querySelectorAll(".user-answer-info"); //Selecting all the "question" buttons at the top corner
+const popUpList = document.querySelectorAll(".pop-up-info"); //Selecting all pop-ups with additional info
+infoButtonList.forEach((infoButton,i) => { //Adding the same event to all "question" button
+    infoButton.addEventListener("click",() =>{ //When we click on a corner "question" button
+        popUpList[i].classList.remove("hidden"); //A corresponding pop-up appears on the screen
     });
 });
-const popUpBtnList = document.querySelectorAll(".pop-up-btn-close");
-popUpBtnList.forEach(
-    (popUpBtn,i) => {
-        popUpBtn.addEventListener("click", () => {
-            popUpList[i].classList.add("hidden");
+const popUpBtnList = document.querySelectorAll(".pop-up-btn-close");//Selectig all closing buttons on pop-ups
+popUpBtnList.forEach( //Adding the same event to all closing buttons
+    (popUpBtn) => {
+        popUpBtn.addEventListener("click", () => { //When we click on a closing button
+            popUpBtn.parentElement.classList.add("hidden"); //The parent pop-up will be hidden
         });
     }
 );
+let currentEmotion;
+let currentEmotionData;
+const emotionList = document.querySelectorAll(".emotion-selection");
+const emotionDataList = document.querySelectorAll(".table-emotions-data");
+emotionList.forEach((emotion) => {
+    emotion.addEventListener("click",() => {
+        currentEmotion = emotion.textContent;
+        console.log(currentEmotion);
+        emotion.parentElement.classList.add("hidden");
+        emotionDataList[currentEmotionData].textContent = currentEmotion;
+    });
+});
+emotionDataList.forEach((emotionData,i) => {
+    emotionData.addEventListener("click", () =>{
+        document.querySelector(".pop-up-emotions").classList.remove("hidden");
+        emotionData.classList.add("is-chosen");
+        currentEmotionData = i;
+        console.log(currentEmotionData);
+    });
+});
+
 

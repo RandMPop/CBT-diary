4a96329bc075720cd9f65609838d9800bccc6e9c
RandMPop <OlgaCherniaeva99@yandex.ru>
2024-09-06 14:00:56 +0500

    Infomation element for cognitive distortion was added. A click on a button for information will not close the whole pop-up.

--- scripts/dairy-functions.js
+++ scripts/dairy-functions.js
@@ -13,73 +13,40 @@ popUpBtnList.forEach( //Adding the same event to all closing buttons
         });
     }
 );
-let currentEmotion; //A variable for chosen emotion
-let currentEmotionData; //A variavle for chosen table cell in the Emotion table
-const emotionList = document.querySelectorAll(".emotion-selection"); //Selecting all emotion options to add an event
-const emotionDataList = document.querySelectorAll(".table-emotions-data"); //Selecting all table cells for emotions to add an event
-const popUpEmotion = document.querySelector(".pop-up-emotions");
-optionChoice(popUpEmotion,emotionList,emotionDataList);
-// emotionList.forEach((emotion) => {//Adding an event to all emotion options
-//     emotion.addEventListener("click",() => { //When we click on the emotion option
-//         currentEmotion = emotion.textContent; //the text of the chosen emotion will be saved in a variable
-//         emotion.parentElement.classList.add("hidden"); //the pop-up with emotion selection will be closed
-//         emotionDataList[currentEmotionData].textContent = currentEmotion; //the cell chosen by the user will be filled with chosen emotion.
-//     });
-// });
-// emotionDataList.forEach((emotionData,i) => { //Adding an event to all table cells with for emotions
-//     emotionData.addEventListener("click", () =>{ ////When we click on a table cell
-//         document.querySelector(".pop-up-emotions").classList.remove("hidden"); //A pop-up with emotion options will appear
-//         emotionData.classList.add("is-chosen");//Style of the chosen cell will be slightly changed
-//         currentEmotionData = i; //The position of the cell will be saved in order to put a chosen emotion in it
-//     });
-// });
-let currentDistortion;
-let currentDistortionRow;
-const distortionList = document.querySelectorAll(".distortion-selection");
-// const distortionList = document.querySelectorAll(".distortion-selection span");
-const distortionRowList = document.querySelectorAll(".table-distortion-row");
-const popUpDistortion = document.querySelector(".pop-up-distortions");
-optionChoice(popUpDistortion,distortionList,distortionRowList);
 
-function optionChoice(popUp,elementList,dataList) {
-    let currentElement,currentData;
-    elementList.forEach((element) => {
-        element.addEventListener("click", () => {
-            currentElement = element.querySelector("span").textContent;
-            // currentElement = element.textContent;
-            element.parentElement.classList.add("hidden");
-            dataList[currentData].textContent = currentElement;
+function optionChoice(popUp,elementList,dataList) { 
+    //A function that allows to choose an option on the pop-up and add it to the table
+    let currentElement,currentData; //Variables for the chosen option and the clicked table cell or row
+    elementList.forEach((element) => { //An event is added for every option i.e. emotion, distortion
+        element.addEventListener("click", () => { //When we click on an option
+            // currentElement = element.querySelector("span").textContent; //It't text will be saved in a variable
+            currentElement = element.textContent;
+            // element.parentElement.classList.add("hidden"); //parent pop-up will be closed
+            popUp.classList.add("hidden");
+            dataList[currentData].textContent = currentElement;//the chosen cell will be filled with the chosen option
         });
     });
-    dataList.forEach((data,i) => {
-        data.addEventListener(("click"),() => {
-            popUp.classList.remove("hidden");
-            data.classList.add(".is-chosen");
-            currentData = i;
+    dataList.forEach((data,i) => {//An event is added for every rable cell or row
+        data.addEventListener(("click"),() => {//When we click on a cell
+            popUp.classList.remove("hidden");//A corresponding pop-up will appear
+            data.classList.add("is-chosen");//A filled cell's style will be slightly changed
+            currentData = i; //A chosen cell is saved in a variable so it can be filled with chosen option
         });
     });
 }
 
-// let works = optionChoice(popUpDistortion,distortionList,distortionRowList);
-// console.log(works);
+const emotionList = document.querySelectorAll(".emotion-selection"); //Selecting all emotion options to add an event
+const emotionDataList = document.querySelectorAll(".table-emotions-data"); //Selecting all table cells for emotions to add an event
+const popUpEmotion = document.querySelector(".pop-up-emotions"); //Selecting the pop-up with emotions
+optionChoice(popUpEmotion,emotionList,emotionDataList); //Calling a function to fill table cells with chosen content
 
-// function optionChoice (popUp,elementList,dataList) {
-//     let currentData, currentElement;
-//     elementList.forEach((element) => {
-//         element.addEventListener("click", () => {
-//             currentElement = element.textContent;
-//             element.parentElement.classList.add("hidden");
-//             dataList[currentData].textContent = currentElement;
-//         });
-//     });
-//     dataList.forEach((data,i) => {
-//         data.addEventListener(("click"),() => {
-//             popUp.classList.remove("hidden");
-//             data.classList.add(".is-chosen");
-//             currentData = i;
-//         });
-//     });
-//     return 1;
-// }
+// const distortionList = document.querySelectorAll(".distortion-selection");//Selecting all distortion options to add an event
+const distortionList = document.querySelectorAll(".distortion-selection span");
+const distortionRowList = document.querySelectorAll(".table-distortion-row"); 
+//Selecting all table rows for distortions to add an event
+const popUpDistortion = document.querySelector(".pop-up-distortions"); //Selecting the pop-up with distortions
+optionChoice(popUpDistortion,distortionList,distortionRowList); //Calling a function to fill table rows with chosen content
+
+
 
 
